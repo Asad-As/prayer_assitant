8841c13aad5e8cb14a4b9622fdfffc09953bac98
naveedrahman <116733146+naveedrahman@users.noreply.github.com>
2022-11-11 04:56:03 -0800

    color added

--- lib/Core/AppColors.dart
+++ lib/Core/AppColors.dart
@@ -5,4 +5,5 @@ class AppColors{
   static const lightGreyColor = Color(0xFFF5F5F5);
   static const darkGreyColor = Colors.grey;
   static const blackColor = Color(0xFF0B0B0C);
+  static const greenColor = Color(0xFF00B644);
 }
