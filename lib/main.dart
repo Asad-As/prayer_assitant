485db956134f0d15950399ffe4016b9479eda24b
Asad-As <83602786+Asad-As@users.noreply.github.com>
2022-11-11 04:44:12 -0800

    push

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         builder: (context, child)
         {
           return MaterialApp(
-            title: 'Flutter  hello  i thiml  world Demo',
+            title: 'Flutter  hello world',
             theme: ThemeData(
               primarySwatch: Colors.blue,
             ),
