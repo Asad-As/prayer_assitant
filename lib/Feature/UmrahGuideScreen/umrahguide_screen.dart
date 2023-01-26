d3c604a63a056bdba32db2f10cc97eef6e32d6cb
Asad-As <83602786+Asad-As@users.noreply.github.com>
2023-01-26 20:56:44 +0500

    final app

--- lib/Feature/UmrahGuideScreen/umrahguide_screen.dart
+++ lib/Feature/UmrahGuideScreen/umrahguide_screen.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -99,6 +100,51 @@ class _UmrahGuideScreenState extends State<UmrahGuideScreen> {
       )
 
 
+    );
+  }
+}
+*/
+
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+
+/// Represents Homepage for Navigation
+class Umrahguid extends StatefulWidget {
+  @override
+  _HomePage createState() => _HomePage();
+}
+
+class _HomePage extends State<Umrahguid> {
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Syncfusion Flutter PDF Viewer'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.bookmark,
+              color: Colors.white,
+              semanticLabel: 'Bookmark',
+            ),
+            onPressed: () {
+              _pdfViewerKey.currentState?.openBookmarkView();
+            },
+          ),
+        ],
+      ),
+      body: SfPdfViewer.network(
+        'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf',
+        key: _pdfViewerKey,
+      ),
     );
   }
 }
